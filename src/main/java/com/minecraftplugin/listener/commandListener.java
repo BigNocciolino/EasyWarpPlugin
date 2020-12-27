90cfecc940431a9fdd3c92cfe7ae4840bfcd7562
Pepegabruh <alfonsosorrentino211@gmail.com>
2020-12-27 17:44:19 +0100

    Added the first two commands

--- src/main/java/com/minecraftplugin/listener/commandListener.java
+++ src/main/java/com/minecraftplugin/listener/commandListener.java
@@ -1,15 +1,31 @@
 package com.minecraftplugin.listener;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class commandListener implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player p = (Player) sender;
+        Location lo = p.getLocation();
+        String wpName;
 
+        if (sender instanceof Player) {
+            if (command.equals("setwarp")) {
+
+            }
+
+            if (command.equals("warp")) {
+
+            }
+        }else {
+            System.out.println("Non puoi eseguire il comando da console");
+        }
 
         return false;
     }
