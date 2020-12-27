ff507662db4272b5e2a99ff272456e692acc5ad9
Pepegabruh <alfonsosorrentino211@gmail.com>
2020-12-27 18:11:00 +0100

    Initial commit

--- src/main/java/com/minecraftplugin/listener/commands/setWarp.java
+++ src/main/java/com/minecraftplugin/listener/commands/setWarp.java
@@ -1,5 +1,25 @@
 package com.minecraftplugin.listener.commands;
 
-public class setWarp {
+import com.minecraftplugin.minecraftplugin.Main;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
+public class setWarp {
+     private Player p;
+     private Location lo;
+     private String wpName;
+
+    //Constructior
+    public setWarp(Player p, Location lo, String wpname) {
+        this.p = p;
+        this.lo = lo;
+        this.wpName = wpname;
+    }
+
+    //Main method for setwarp
+    public void stWarp() {
+        if (Main.getData().warpExist(wpName)) {
+            Main.getData().saveData(p, lo, wpName);
+        }
+    }
 }
