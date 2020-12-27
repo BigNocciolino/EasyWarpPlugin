f58e7cac83b4904db787cc511afa6bd8b0019b9e
Pepegabruh <alfonsosorrentino211@gmail.com>
2020-12-27 17:44:59 +0100

    improvements

--- src/main/java/com/minecraftplugin/minecraftplugin/Main.java
+++ src/main/java/com/minecraftplugin/minecraftplugin/Main.java
@@ -1,22 +1,33 @@
 package com.minecraftplugin.minecraftplugin;
 
+import com.minecraftplugin.data.data;
+import com.minecraftplugin.listener.commandListener;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class Main extends JavaPlugin {
 
     public static Main plugin;
+    public static data data;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
         System.out.println("The pllugin was started successfully");
+        this.getCommand("setwarp").setExecutor(new commandListener());
+        this.getCommand("warp").setExecutor(new commandListener());
+        data = new data();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         System.out.println("The pllugin was stopped successfully");
+    }
+
+    public static data getData() {
+        return data;
     }
 
     public static Main getInstance() {
