1233f16807064619711bd35e0530f7d3e04338b4
Pepegabruh <alfonsosorrentino211@gmail.com>
2020-12-29 21:41:07 +0100

    Fixed some bugs

--- src/main/java/com/minecraftplugin/listener/commandListener.java
+++ src/main/java/com/minecraftplugin/listener/commandListener.java
@@ -3,12 +3,17 @@ package com.minecraftplugin.listener;
 import com.minecraftplugin.listener.commands.delWarp;
 import com.minecraftplugin.listener.commands.setWarp;
 import com.minecraftplugin.listener.commands.warp;
+import com.minecraftplugin.listener.commands.warplist;
 import com.minecraftplugin.minecraftplugin.Main;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class commandListener implements CommandExecutor {
 
@@ -19,32 +24,64 @@ public class commandListener implements CommandExecutor {
 
         Player p = (Player) sender;
         Location lo = p.getLocation();
-        String wpName = args[0];
 
         if (sender instanceof Player) {
             if (command.getName().equals("setwarp")) {
-                setWarp setWarp = new setWarp(p, lo, wpName);
-                if (Main.getData().warpExist(wpName)) {
-                    setWarp.stWarp();
-                    sender.sendMessage(pluginName + "§6 Succesfully set the warp §6");
+                if (args.length >= 1) {
+                    String wpName = args[0];
+                    setWarp setWarp = new setWarp(p, lo, wpName);
+                    if (Main.getData().warpExist(wpName)) {
+                        setWarp.stWarp();
+                        sender.sendMessage(pluginName + "§6 Succesfully set the warp §6");
+                    } else {
+                        sender.sendMessage(pluginName + "§6 The warp already exists");
+                    }
                 }else {
-                    sender.sendMessage("§6Il warp esiste§6");
+                    sender.sendMessage(pluginName + "§6 The argument must be present§6");
                 }
             }
 
             if (command.getName().equals("warp")) {
-                warp warp = new warp(p, wpName);
-                warp.warpYourself();
-                sender.sendMessage(pluginName + "§6 You warped yourself §6");
+                if (args.length >= 1) {
+                    String wpName = args[0];
+                    warp warp = new warp(p, wpName);
+                    if (Main.getData().warpExist(wpName)!=true) {
+                        warp.warpYourself();
+                        sender.sendMessage(pluginName + "§6 You warped yourself §6");
+                    }else{
+                        sender.sendMessage(pluginName + "§6 There is no warp with this name");
+                    }
+                }else {
+                    sender.sendMessage(pluginName + "§6 The argument must be present");
+                }
             }
 
             if (command.getName().equals("delwarp")) {
-                delWarp delWarp = new delWarp(wpName);
-                delWarp.delwarp();
-                sender.sendMessage(pluginName + "§6 Succesfully deleted the warp §6");
+                if (args.length >= 1) {
+                    String wpName = args[0];
+                    delWarp delWarp = new delWarp(wpName);
+                    delWarp.delwarp();
+                    sender.sendMessage(pluginName + "§6 Succesfully deleted the warp §6");
+                }else {
+                    sender.sendMessage(pluginName + "§6 The argument must be present");
+                }
             }
+
+            if (command.getName().equals("warps")) {
+                warplist warplist = new warplist();
+                List<String> warps = warplist.returnWarpList();
+
+                if (warplist.returnWarpList()!=null) {
+                    for (String x : warps) {
+                        sender.sendMessage(pluginName + " §6Name: " + "§b" + x + " §6Owner: §b" + Main.getData().returnOwner(x));
+                    }
+                }else {
+                    sender.sendMessage(pluginName + "§6 There is no warp in the list§6");
+                }
+            }
+
         }else {
-            System.out.println("§6Non puoi eseguire il comando da console§6");
+            System.out.println("Non puoi eseguire il comando da console");
         }
 
         return false;
